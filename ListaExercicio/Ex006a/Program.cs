57ccf08f464d003c239c0d6a34dc94cce7c344e9
Auto Logon <autologon@CSED.LAB>
2024-08-12 21:46:25 -0300

    c#

--- ListaExercicio/Ex006a/Program.cs
+++ ListaExercicio/Ex006a/Program.cs
@@ -2,4 +2,34 @@
 //2 - Criar um laço para percorrer o vetor.
 //3 - Gerar um valor aleatório para cada posição.
 //4 - Ordenar o vetor Bubble Sort. 
-//5 -
+
+int tamanho = 100;
+int [] vetor = new int [tamanho];
+Random random = new Random();
+
+for (int i = 0; i < tamanho; i++) //for (int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = random.Next(0, 100); //vetor[i] = random.Next(100);
+}
+
+Console.WriteLine("");
+
+//Ordenação do vetor com Bubble Sort
+for (int i = 0; i < tamanho - 1; i++)
+{
+    if (vetor[i] > vetor[i+1])
+    {
+        int aux = vetor[i];
+        vetor[i] = vetor[i+1];
+        vetor[i+1] = aux;    
+    }  
+}
+
+//Imprimir o vetor com valores com ordenação
+for (int i = 0; i < tamanho; i++)
+{ 
+    Console.WriteLine("O Número é: " + vetor[i]);
+}
+
+
+
