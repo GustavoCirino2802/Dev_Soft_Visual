3ee4f493f13b7810532934ea40508ee8a3eda90c
Auto Logon <autologon@CSED.LAB>
2024-09-02 20:49:38 -0300

    Web API

--- ListaExercicio/Ex001/Program.cs
+++ ListaExercicio/Ex001/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Ex003()
+    {
+        
+        Console.Write("Digite a altura do retângulo: ");     
+        double altura = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("Digite a largura do retângulo: ");
+        double largura = Convert.ToDouble(Console.ReadLine());
+        
+        double area = altura * largura;
+      
+        Console.WriteLine($"A área do retângulo é: {area}");
+    }
+}
--- ListaExercicio/Ex003/Program.cs
+++ ListaExercicio/Ex003/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-class Program{
+class Program
+{
 
     static void Main(){
 
