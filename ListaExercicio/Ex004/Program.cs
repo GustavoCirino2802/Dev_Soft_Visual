ff1dd610bd4ac24749b106e4558e587a5b86c2a2
Auto Logon <autologon@CSED.LAB>
2024-09-02 20:12:37 -0300

    Ex004

--- ListaExercicio/Ex004/Program.cs
+++ ListaExercicio/Ex004/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(){
+        Console.WriteLine("Digite o seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Digite a sua idade: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        if (idade <= 13)
+        {
+            Console.WriteLine($"O {nome} tem {idade} anos e é criança");
+        }
+
+        else if (idade > 13 && idade < 18)
+        {
+            Console.WriteLine($"O {nome} tem {idade} anos e é adolescente");
+        }
+
+        else if (idade >= 18 && idade < 60)
+        {
+            Console.WriteLine($"O {nome} tem {idade} anos e é Adulto");
+        }
+
+        else 
+        {
+            Console.WriteLine($"O {nome} tem {idade} anos e é Idoso");
+        }
+
+
+}
+}
